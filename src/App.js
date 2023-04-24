16b359eae7c438d85815b97c56bc81d1a67c4873
Gouarv Rattan <gouravrattan11@gmail.com>
2023-04-24 22:22:45 +0530

    project initialization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+
+export default function App() {
+  return ( 
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
+}
